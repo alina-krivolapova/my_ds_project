5d1c6151f480cfca0b1518f1ba66d104d23ae9da
Alina Krivolapova <flameswinda@gmail.com>
2021-06-23 13:34:37 +0300

    Change code structure using classes

--- news_downloader.py
+++ news_downloader.py
@@ -5,65 +5,69 @@ import bs4
 from datetime import datetime
 from typing import List, Dict, Union
 
-from parser_common import download_data_from_site
+from data_provider import DataProvider
 
 PATTERN = "https://www.theguardian.com/technology/elon-musk"
 
 
-def generate_list_of_pages(num_of_pages: int) -> List[str]:
-    """ Create list of pages urls based on pattern."""
-    list_of_pages = list()
-    list_of_pages.append(PATTERN)
-    for i in range(2, num_of_pages + 1):
-        list_of_pages.append(PATTERN + f"?page={i}")
-    return list_of_pages
+class NewsDataProvider(DataProvider):
+    """ Class to work with news data. """
 
+    def __init__(self):
+        self.articles = []
 
-def get_date_like_datetime(text: str) -> datetime:
-    """Return date in datetime format.
+        for url in self.generate_list_of_pages(20):
+            src = self.download_data_from_site(url)
+            soup = bs4.BeautifulSoup(src, "lxml")
 
-    Example:
-        Date text is Mon 14 Jun 2021 01.12 BST"""
-    return datetime.strptime(' '.join(text.split()[1:4]), '%d %b %Y')
+            # save all links to particular news
+            news = set()
+            for item in soup.find_all('a', {'class': re.compile(".* js-headline-text")}):
+                link = item.get('href')
+                if re.search('/202[0-1]', link):
+                    news.add(link)
 
+            # save particular articles
+            for article in news:
+                parsed = self.parse_data(article)
+                if parsed:
+                    self.articles.append(parsed)
 
-def parse_news(url: str) -> Dict[str, Union[str, datetime]]:
-    """Parse data.
+    @staticmethod
+    def generate_list_of_pages(num_of_pages: int) -> List[str]:
+        """ Create list of pages urls based on pattern."""
+        list_of_pages = list()
+        list_of_pages.append(PATTERN)
+        for i in range(2, num_of_pages + 1):
+            list_of_pages.append(PATTERN + f"?page={i}")
+        return list_of_pages
 
-     Returns list of dicts with news
-     Output example:
-     {'date': datetime.datetime(2021, 6, 11, 0, 0), 'news_text': 'It’s famously impossible to take...'}
-     """
-    src = download_data_from_site(url)
-    soup = bs4.BeautifulSoup(src, "lxml")
-    news = dict()
-    # in other articles it's hard to identify date
-    if soup.find("label", attrs={"for": "dateToggle"}):
-        news["date"] = get_date_like_datetime(soup.find("label", attrs={"for": "dateToggle"}).text)
-        news["news_text"] = soup.find("div",
-                                      class_=re.compile(
-                                          "^article-body-commercial-selector article-body-viewer-selector .*")).text
-    return news
+    @staticmethod
+    def get_date_like_datetime(text: str) -> datetime:
+        """Return date in datetime format.
 
+        Example:
+            Date text is Mon 14 Jun 2021 01.12 BST"""
+        return datetime.strptime(' '.join(text.split()[1:4]), '%d %b %Y')
 
-def get_news():
-    """ Function to download and parse news."""
-    articles = []
+    def parse_data(self, url: str) -> Dict[str, Union[str, datetime]]:
+        """Parse data.
 
-    for url in generate_list_of_pages(20):
-        src = download_data_from_site(url)
+        Returns list of dicts with news
+        Output example:
+        {'date': datetime.datetime(2021, 6, 11, 0, 0), 'news_text': 'It’s famously impossible to take...'}
+        """
+        src = self.download_data_from_site(url)
         soup = bs4.BeautifulSoup(src, "lxml")
+        news = dict()
+        # in other articles it's hard to identify date
+        if soup.find("label", attrs={"for": "dateToggle"}):
+            news["date"] = self.get_date_like_datetime(soup.find("label", attrs={"for": "dateToggle"}).text)
+            news["news_text"] = soup.find("div",
+                                          class_=re.compile(
+                                              "^article-body-commercial-selector article-body-viewer-selector .*")).text
+        return news
 
-        # save all links to particular news
-        news = set()
-        for item in soup.find_all('a', {'class': re.compile(".* js-headline-text")}):
-            link = item.get('href')
-            if re.search('/202[0-1]', link):
-                news.add(link)
-
-        # save particular articles
-        for article in news:
-            parsed = parse_news(article)
-            if parsed:
-                articles.append(parsed)
-    return articles
+    def get_data(self):
+        """ Function to provide news."""
+        return self.articles
